11e090ca81be95366b6f1a86e83bd59d1a3f03f7
Gabriel Coimbra De Oliveira Da Silva <gabriel.oliveira@circulosaude.com.br>
2025-07-10 12:57:22 -0300

    Implement user creation logic and define request/response models

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
 
+	"github.com/coimbrox/MeuPrimeiroCRUDGo/src/controller/routes"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -14,6 +14,12 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
+	router := gin.Default()
 
-	fmt.Println(os.Getenv("TEST"))
+	routes.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
+
 }
--- src/controller/user/createUser.go
+++ src/controller/user/createUser.go
@@ -1,5 +1,25 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
 
-func CreateUser(c *gin.Context) {}
+	"github.com/coimbrox/MeuPrimeiroCRUDGo/src/controller/model/request"
+	"github.com/coimbrox/MeuPrimeiroCRUDGo/src/rest_err"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context) {
+
+		var userRequest request.UserRequest
+
+		if err := c.ShouldBindJSON(&userRequest); err != nil {
+			restErr := rest_err.NewBadRequestError(
+				fmt.Sprintf("There are some incorrect fields, error =%s",err.Error()))
+
+				c.JSON(restErr.Code,restErr)
+				return
+		}
+
+		fmt.Println(userRequest)
+
+}
