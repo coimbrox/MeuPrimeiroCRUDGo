f8a65cc84ec7cb13d0cf7749019f96178f1ab16f
Gabriel Coimbra De Oliveira Da Silva <gabriel.oliveira@circulosaude.com.br>
2025-07-11 12:48:49 -0300

    Add validation tags to UserRequest fields for improved input handling

--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -1,8 +1,9 @@
 package request
 
 type UserRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Name     string `json:"name"`
-	Age      int    `json:"age"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6,max=20,containsany=!@#$%^&*()"`
+	Username string `json:"username" binding:"required,min=3,max=20"`
+	Name     string `json:"name" binding:"required,min=3,max=80"`
+	Age      int    `json:"age" binding:"required,min=1,max=140"`
 }
