20427340a70d4459be62189fd8bc3b6083dd9663
Gabriel Coimbra De Oliveira Da Silva <gabriel.oliveira@circulosaude.com.br>
2025-07-11 13:12:06 -0300

    Refactor CreateUser controller to improve error handling and add validation logic

--- src/controller/user/createUser.go
+++ src/controller/user/createUser.go
@@ -2,21 +2,23 @@ package controller
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/coimbrox/MeuPrimeiroCRUDGo/src/configuration/validation"
 	"github.com/coimbrox/MeuPrimeiroCRUDGo/src/controller/model/request"
-	"github.com/coimbrox/MeuPrimeiroCRUDGo/src/rest_err"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-
+	log.Println("CreateUser controller called")
 		var userRequest request.UserRequest
 
 		if err := c.ShouldBindJSON(&userRequest); err != nil {
-			restErr := rest_err.NewBadRequestError(
-				fmt.Sprintf("There are some incorrect fields, error =%s",err.Error()))
+			log.Printf("Error trying to marshal object,error=%s\n", err.Error())
+			errRest := validation.ValidateUserError(err)
+		
 
-				c.JSON(restErr.Code,restErr)
+				c.JSON(errRest.Code,errRest)
 				return
 		}
 
