3ce2f57b75d895fa1ca839d339dd9b7eaed4e92d
Gabriel Coimbra De Oliveira Da Silva <gabriel.oliveira@circulosaude.com.br>
2025-07-14 12:43:15 -0300

    Add logging functionality with zap and update CreateUser controller for improved logging

--- src/controller/user/createUser.go
+++ src/controller/user/createUser.go
@@ -2,25 +2,33 @@ package controller
 
 import (
 	"fmt"
-	"log"
 
+	"github.com/coimbrox/MeuPrimeiroCRUDGo/src/configuration/logger"
 	"github.com/coimbrox/MeuPrimeiroCRUDGo/src/configuration/validation"
 	"github.com/coimbrox/MeuPrimeiroCRUDGo/src/controller/model/request"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
-	log.Println("CreateUser controller called")
+logger.Info("Init CreateUser controller",
+		zap.String("journey", "createUser"),
+	)
 		var userRequest request.UserRequest
 
 		if err := c.ShouldBindJSON(&userRequest); err != nil {
-			log.Printf("Error trying to marshal object,error=%s\n", err.Error())
+			logger.Error("Error trying to validate user info", err,
+			zap.String("journey", "createUser"))
 			errRest := validation.ValidateUserError(err)
 		
 
 				c.JSON(errRest.Code,errRest)
 				return
 		}
+
+		logger.Info(
+		"CreateUser controller executed successfully",
+		zap.String("journey", "createUser"))
 
 		fmt.Println(userRequest)
 
